50a43680c31d97ac9b31dc26b1b7668cff8cb519
eryk <eryk.czyzak@gmail.com>
2024-08-18 13:50:37 +0200

    Deployer calculations, fixed bug with displaying "Infinity"

--- src/app/calculator/deployer-calculator/deployer-calculator.component.ts
+++ src/app/calculator/deployer-calculator/deployer-calculator.component.ts
@@ -2,35 +2,49 @@ import { ChangeDetectorRef, Component, inject } from '@angular/core';
 import { NumberComponent } from "../shared/number/number.component";
 import { OutputSideComponent } from "../shared/output-side/output-side.component";
 import { InputSideComponent } from "../shared/input-side/input-side.component";
-import { DeployerCalculator, Result } from './deployer.calculator';
+import { DeployerCalculator, Process, Result } from './deployer.calculator';
+import { EnumComponent } from "../shared/enum/enum.component";
+import { NuenumComponent } from "../shared/nuenum/nuenum.component";
 
 @Component({
   selector: 'app-deployer-calculator',
   standalone: true,
-  imports: [NumberComponent, OutputSideComponent, InputSideComponent],
+  imports: [NumberComponent, OutputSideComponent, InputSideComponent, EnumComponent, NuenumComponent],
   templateUrl: './deployer-calculator.component.html',
   styleUrl: './deployer-calculator.component.css'
 })
 export class DeployerCalculatorComponent {
+  Process = Process;
   val_rpm: number = 256;
   val_time: number = 0;
   val_speed: number = 0;
+  in_process: Process = 0;
+  in_health: number = 10;
+  in_damage: number = 4;
   private cdRef = inject(ChangeDetectorRef);
+
+  get processes(): string[] {
+    return [
+      "Belt processing",
+      "Killing",
+      "Using/Placing/Inserting item"
+    ]
+  }
 
   ngOnInit() {
     this.calculateFromRpm();
   }
 
   calculateFromRpm() {
-    this.updateValues(DeployerCalculator.calculateFromRpm(this.val_rpm));
+    this.updateValues(DeployerCalculator.calculateFromRpm(this.val_rpm, this.in_process, this.in_health, this.in_damage));
   }
 
   calculateFromSpeed() {
-    this.updateValues(DeployerCalculator.calculateFromSpeed(this.val_speed));
+    this.updateValues(DeployerCalculator.calculateFromSpeed(this.val_speed, this.in_process, this.in_health, this.in_damage));
   }
 
   calculateFromTime() {
-    this.updateValues(DeployerCalculator.calculateFromTime(this.val_time));
+    this.updateValues(DeployerCalculator.calculateFromTime(this.val_time, this.in_process, this.in_health, this.in_damage));
   }
 
   updateValues(result: Result) {
--- src/app/calculator/deployer-calculator/deployer.calculator.ts
+++ src/app/calculator/deployer-calculator/deployer.calculator.ts
@@ -1,12 +1,33 @@
 import { clamp, decimal } from "../constants";
 
 export abstract class DeployerCalculator {
-  public static calculateFromRpm(rpm: number): Result {
-    if(rpm > 0) {
+  public static calculateFromRpm(rpm: number, process: Process, health: number, damage: number): Result {
+    if(rpm > 0 && damage > 0) {
+      let timerSpeed = Math.floor(clamp(rpm * 2, 8, 512));
+
+      let framesWaiting = Math.ceil(500 / timerSpeed) + 1;
+      let framesExpanding = Math.ceil(1000 / timerSpeed) + 1;
+      let framesRetracting = Math.ceil(1000 / timerSpeed) + 1;
+
+      let totalFrames = 0;
+      switch(process) {
+        case Process.BELT_PROCESSING:
+          totalFrames = framesExpanding + framesRetracting;
+          break;
+        case Process.USING_ITEM:
+          totalFrames = framesWaiting + framesExpanding + framesRetracting;
+          break;
+        case Process.KILLING:
+          let hits = Math.ceil(health / damage);
+          totalFrames = hits * (framesWaiting + framesExpanding + framesRetracting);
+          totalFrames += 10 + 1; // output overflow items
+          break;
+      }
+
       return {
         rpm: rpm,
-        time: 0,
-        speed: 0
+        time: totalFrames/20,
+        speed: 1/(totalFrames/20)
       };
     }
     else {
@@ -18,24 +39,24 @@ export abstract class DeployerCalculator {
     }
   }
 
-  public static calculateFromTime(time: number): Result {
-    let resultRpm = this.searchForRpm(0, 1024, time);
-    let result = this.calculateFromRpm(resultRpm);
+  public static calculateFromTime(time: number, process: Process, health: number, damage: number): Result {
+    let resultRpm = this.searchForRpm(0, 1024, time, process, health, damage);
+    let result = this.calculateFromRpm(resultRpm, process, health, damage);
     return result;
   }
 
-  public static calculateFromSpeed(speed: number): Result {
-    let result = this.calculateFromTime(1/speed);
+  public static calculateFromSpeed(speed: number, process: Process, health: number, damage: number): Result {
+    let result = this.calculateFromTime(1/speed, process, health, damage);
     return result;
   }
 
-  private static searchForRpm(rpm: number, maxRpm: number, targetTime: number): number {
-    let time = this.calculateFromRpm(rpm).time;
+  private static searchForRpm(rpm: number, maxRpm: number, targetTime: number, process: Process, health: number, damage: number): number {
+    let time = this.calculateFromRpm(rpm, process, health, damage).time;
     if(time <= targetTime || rpm >= maxRpm) {
       return rpm;
     }
     else {
-      return this.searchForRpm(rpm+1, maxRpm, targetTime);
+      return this.searchForRpm(rpm+1, maxRpm, targetTime, process, health, damage);
     }
   }
 }
@@ -44,4 +65,10 @@ export interface Result {
   rpm: number,
   time: number,
   speed: number
+}
+
+export enum Process {
+  BELT_PROCESSING,
+  KILLING,
+  USING_ITEM
 }

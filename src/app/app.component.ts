781e9aa9ac327124cb1b8fba85c0e3f1ce00091e
eryk <eryk.czyzak@gmail.com>
2024-08-25 09:34:52 +0200

    Enums and Nuenums, changed layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { CalculatorSideComponent } from "./calculator/calculator-side.component";
-import { ContentComponent } from './content/content.component';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, CalculatorSideComponent, ContentComponent, FormsModule],
+  imports: [RouterOutlet, HeaderComponent, CalculatorSideComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/calculator/calculator-side.component.ts
+++ src/app/calculator/calculator-side.component.ts
@@ -30,4 +30,7 @@ export class CalculatorSideComponent {
     })
   }
 
+  get calculator() {
+    return this.calculatorService.getSelectedCalculator();
+  }
 }

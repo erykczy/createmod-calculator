59b32a1b0fda2c65c513a6d8e46bdd27939fdda9
eryk <eryk.czyzak@gmail.com>
2024-08-27 11:42:38 +0200

    Synchronized icon and calculator fade-in animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterOutlet } from '@angular/router';
-import { CalculatorInfoComponent } from "./calculator/calculator-info.component";
+import { ActivationStart, Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { CalculatorService } from './calculator/calculator.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, CalculatorInfoComponent, FormsModule],
+  imports: [RouterOutlet, HeaderComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'createmod-calculator';
+  switchTabAnimation: boolean = false;
+  private calculatorService = inject(CalculatorService);
+  private router = inject(Router);
+
+  ngOnInit() {
+    this.router.events.subscribe((val) => {
+      if(val instanceof ActivationStart) {
+        this.calculatorService.setSelectedCalculator(val.snapshot.url[0].path);
+        this.onCalculatorChanged();
+      }
+    })
+  }
+
+  get calculator() {
+    return this.calculatorService.getSelectedCalculator();
+  }
+
+  onCalculatorChanged() {
+    this.switchTabAnimation = true;
+  }
+
+  onAnimationend() {
+    this.switchTabAnimation = false;
+  }
 }
